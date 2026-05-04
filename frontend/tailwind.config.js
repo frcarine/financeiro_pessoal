34484ea0296bfacb2f778e230d457556f72467f2
ps.carine <carine.ponte.ferreira@gmail.com>
2026-05-03 21:36:05 -0400

    Fix Tailwind brand color scale

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         brand: {
           50: '#eef2ff',
+          100: '#e0e7ff',
           500: '#6366f1',
           600: '#4f46e5',
           700: '#4338ca'
